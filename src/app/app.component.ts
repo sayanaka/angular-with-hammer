4d1546b25f39212778e6c6f42c28fe611352bd9a
sayanaka <sayanaka709@gmail.com>
2019-02-23 15:39:52 +0900

    Edit app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularWithHammer';
+  index = 0;
+  contentArray = ['１番目のテキスト', '２番目のテキスト', '３番目のテキスト'];
+
+  hammerEventType: string;
+
+  /** hammerJSのイベントハンドラ */
+  hammerEvent(event: any) {
+    this.hammerEventType = event.type;
+
+    if (event.type === 'swipeleft' && this.index > 0) {
+      this.index--;
+      return;
+    }
+
+    if (
+      event.type === 'swiperight' &&
+      this.index < this.contentArray.length - 1
+    ) {
+      this.index++;
+      return;
+    }
+  }
 }
