2e8fc0868f491b9edfcd08e02dff64ab45fa4c9e
sayanaka <sayanaka709@gmail.com>
2019-02-23 15:49:34 +0900

    Create "twosecondspress" Event

--- src/app/my-hammer-config.ts
+++ src/app/my-hammer-config.ts
@@ -1,6 +1,9 @@
 import { HammerGestureConfig } from '@angular/platform-browser';
 
 export class MyHammerConfig extends HammerGestureConfig {
+  /** 追加したいイベント名 */
+  events: string[] = ['twosecondspress'];
+
   /** buildHammer */
   buildHammer(element: HTMLElement) {
     const hammer =
@@ -10,6 +13,17 @@ export class MyHammerConfig extends HammerGestureConfig {
     for (const eventName of Object.keys(this.overrides)) {
       mc.get(eventName).set(this.overrides[eventName]);
     }
+
+    // ２秒長押しイベントの作成
+    const press = new hammer.Press();
+    const twoSecondsPress = new hammer.Press({
+      event: 'twosecondspress',
+      time: 2000
+    });
+    twoSecondsPress.recognizeWith(press);
+    press.requireFailure(twoSecondsPress);
+
+    mc.add(twoSecondsPress);
 
     return mc;
   }
